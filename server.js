9ac01c5670701eeff07b0c59afb3c2408574b3c9
Shiv deshmukh <shivdeshmukh2004@gmail.com>
2024-11-25 22:46:51 +0530

    middleware added

--- server.js
+++ server.js
@@ -19,8 +19,47 @@ const uri = `${dbPrefix}${dbUser}:${dbPwd}${dbUrl}${dbParams}`; // create uri   
 let client = new MongoClient(uri, { serverApi: ServerApiVersion.v1}); // create mongo client
 let db = client.db(dbName); // create db
 
+// Console log MongoDB connection status
+client
+  .connect()
+  .then(async () => {
+    console.log("MongoDB connected successfully");
 
-client.connect().then(() => {async () => {console.log("Connected to the database");}})
+    // Create text index on 'subject' and 'location etc'
+    try {
+      const lessons = db.collection("lessons");
+      const indexResult = await lessons.createIndex({
+        subject: "text",
+        location: "text",
+      });
+    } catch (indexError) {
+      console.error("Error creating index:", indexError);
+    }
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed", err);
+    process.exit(1); // Exit the process if the database connection fails
+  });
+
+app.set("json spaces", 3);
+
+// Static file for lesson images with CORS headers
+const imagePath = path.resolve(__dirname, "images");
+app.use("/images", (req, res, next) => {
+  const fileRequested = path.join(imagePath, req.path);
+  // Check if the file exists
+  fs.access(fileRequested, fs.constants.F_OK, (err) => {
+    if (err) {
+      // File does not exist, return error message
+      res.status(404).json({ error: "Image not found" });
+    } else {
+      // File exists, serve it with CORS headers
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.sendFile(fileRequested);
+    }
+  });
+});
+// Middleware
 
 app.use(cors()); // enable cors
 app.use(morgan("dev")); // enable morgan
